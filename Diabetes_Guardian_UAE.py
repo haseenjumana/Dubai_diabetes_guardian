5cb121460ec711e84d38b5e0ddec41717a028c76
haseenjumana <152371892+haseenjumana@users.noreply.github.com>
2025-04-29 22:12:11 +0700

    Update Diabetes_Guardian_UAE.py

--- Diabetes_Guardian_UAE.py
+++ Diabetes_Guardian_UAE.py
@@ -79,4 +79,5 @@ async def analyze(request: Request,
 
 
 if __name__ == "__main__":
-    uvicorn.run("Diabetes_Guardian_UAE:app", host="127.0.0.1", port=8000, reload=True)
+   uvicorn.run("Diabetes_Guardian_UAE:app", host="0.0.0.0", port=8000)
+
